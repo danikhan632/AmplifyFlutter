709f6665ea0679e19b62e4a4c911020709154bd5
danikhan632 <danikhan632@gmail.com>
2022-08-25 19:40:04 -0400

    deploy

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "yeezus31b14d6b0": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "yeezus3": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import './components/auth_credientials.dart';
 import './route_generate.dart';
@@ -10,24 +12,30 @@ import "package:my_api/my_api.dart";
 import 'dart:core';
 import 'package:flutter/material.dart';
 
+import 'amplifyconfiguration.dart';
+
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-
-
   const MyApp({Key? key}) : super(key: key);
 
 
+  Future<void> _configureAmplify() async {
+    try {
+      await Amplify.addPlugin(AmplifyAuthCognito());
+      await Amplify.configure(amplifyconfig);
+    } on Exception catch (e) {
+      print('Could not configure Amplify: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  const MaterialApp(
     initialRoute: '/',
     onGenerateRoute: RouteGenerator.generateRoute,
     );
