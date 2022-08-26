cf78c8c89a47ac28d0fb1f79d3b2707c91a7c393
danikhan632 <danikhan632@gmail.com>
2022-08-25 23:12:58 -0400

    fixed issue3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,12 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "function": {
-        "yeezus31b14d6b0": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
     "api": {
         "yeezus3": {
             "GraphQLAPIKeyOutput": "string",
--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,9 @@ import './screens/loginPage.dart';
 import './screens/signUp.dart';
 import './components/auth_service.dart';
 import "package:my_api/my_api.dart";
-
 import 'dart:core';
 import 'package:flutter/material.dart';
-
-import './amplifyconfiguration.dart';
+// import './amplifyconfiguration.dart';
 
 
 void main() async{
@@ -25,6 +23,110 @@ class MyApp extends StatelessWidget {
 
 
   Future<void> _configureAmplify() async {
+    const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "analytics": {
+        "plugins": {
+            "awsPinpointAnalyticsPlugin": {
+                "pinpointAnalytics": {
+                    "appId": "0d601214f6bc46cb9a16ecfb354b9784",
+                    "region": "us-east-1"
+                },
+                "pinpointTargeting": {
+                    "region": "us-east-1"
+                }
+            }
+        }
+    },
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "yeezus2": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://r2z6ytm2xjde7oizuypqrbmivq.appsync-api.us-east-1.amazonaws.com/graphql",
+                    "region": "us-east-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-4345ehroprddbdbkhoggxemiw4"
+                },
+                "yeezus3": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://kd7vwpwdlfeajec3d2ejqnsphu.appsync-api.us-east-1.amazonaws.com/graphql",
+                    "region": "us-east-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-m56marwycreoximgndnv54rhya"
+                }
+            }
+        }
+    },
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "AppSync": {
+                    "Default": {
+                        "ApiUrl": "https://r2z6ytm2xjde7oizuypqrbmivq.appsync-api.us-east-1.amazonaws.com/graphql",
+                        "Region": "us-east-1",
+                        "AuthMode": "API_KEY",
+                        "ApiKey": "da2-4345ehroprddbdbkhoggxemiw4",
+                        "ClientDatabasePrefix": "yeezus2_API_KEY"
+                    }
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:032c04ab-02e4-4520-9ba3-2e04ab8cd641",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_mcZFovO3L",
+                        "AppClientId": "2geocfurv5sobqlrj8gd0b7vgg",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "PHONE_NUMBER"
+                        ]
+                    }
+                },
+                "PinpointAnalytics": {
+                    "Default": {
+                        "AppId": "0d601214f6bc46cb9a16ecfb354b9784",
+                        "Region": "us-east-1"
+                    }
+                },
+                "PinpointTargeting": {
+                    "Default": {
+                        "Region": "us-east-1"
+                    }
+                }
+            }
+        }
+    }
+}''';
     try {
       await Amplify.addPlugin(AmplifyAuthCognito());
       await Amplify.configure(amplifyconfig);
